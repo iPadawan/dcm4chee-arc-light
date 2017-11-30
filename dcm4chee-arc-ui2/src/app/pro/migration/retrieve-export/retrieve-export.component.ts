fa16cb73ff85c9a2abe7a9596eeaa14657858632
shefki <shralsheki@gmail.com>
2017-11-30 10:25:41 +0100

    adding table data[UI: New retrieve & export page #7671]

--- dcm4chee-arc-ui2/src/app/pro/migration/retrieve-export/retrieve-export.component.ts
+++ dcm4chee-arc-ui2/src/app/pro/migration/retrieve-export/retrieve-export.component.ts
@@ -4,6 +4,8 @@ import {AeListService} from "../../../ae-list/ae-list.service";
 import {AppService} from "../../../app.service";
 import * as _ from 'lodash';
 import {ActivatedRoute} from "@angular/router";
+import {WindowRefService} from "../../../helpers/window-ref.service";
+import {J4careHttpService} from "../../../helpers/j4care-http.service";
 
 @Component({
   selector: 'app-retrieve-export',
@@ -19,11 +21,16 @@ export class RetrieveExportComponent implements OnInit {
     headerTitle;
     submitText;
     showFilter = false;
+    count;
+    attributeFilters: any = {};
+    patients = [];
+    dicomObject;
     constructor(
         private route: ActivatedRoute,
         private service:RetrieveExportService,
         private aeListService:AeListService,
         private mainservice: AppService,
+        private $http:J4careHttpService
     ) { }
 
     ngOnInit(){
@@ -46,8 +53,8 @@ export class RetrieveExportComponent implements OnInit {
     init() {
         this.route.params
             .subscribe((params) => {
-              this.mode = params.mode;
-              switch(this.mode){
+                this.mode = params.mode;
+                switch(this.mode){
                 case "retrieve":
                     this.headerTitle = "Retrieve studies";
                     this.submitText = "RETRIEVE";
@@ -56,10 +63,12 @@ export class RetrieveExportComponent implements OnInit {
                     this.headerTitle = "Export studies";
                     this.submitText = "EXPORT";
                     break;
-              }
-              this.getAes(2);
+                }
+                this.getAes(2);
+                this.initAttributeFilter('Patient', 1);
             });
     }
+
     getAes(retries){
         this.aeListService.getAes().subscribe((aes)=>{
           this.aes = (<any[]>aes).map(ae => {
@@ -75,5 +84,126 @@ export class RetrieveExportComponent implements OnInit {
                   this.getAes(retries - 1);
         });
     }
+
+    onSubmit(object){
+        console.log("onsubmit object",object);
+        if(_.hasIn(object,"id")){
+            switch (object.id){
+                case 'count':
+                    this.getStudiesCount(object.model);
+                break;
+                case 'querie':
+                    this.getStudies(object.model);
+                break;
+            }
+        }else{
+
+        }
+    }
+
+    getStudiesCount(params){
+        this.service.getStudiesCount(params).subscribe((count)=>{
+            console.log("count",count);
+            this.count = count.count;
+        },(err)=>{
+
+        });
+    }
+    extractAttrs(attrs, tags, extracted) {
+        for (let tag in attrs){
+            if (_.indexOf(tags, tag) > -1){
+                extracted[tag] = attrs[tag];
+            }
+        }
+    }
+    getStudies(params){
+        let offset = 0;
+        let $this = this;
+        this.service.getStudies(params).subscribe((res)=>{
+            console.log("studies",res);
+            this.dicomObject = res;
+            //Add number of patient related studies manuelly hex(00201200) => dec(2101760)
+            let index = 0;
+            while ($this.attributeFilters.Patient.dcmTag[index] && ($this.attributeFilters.Patient.dcmTag[index] < 2101760)) {
+                index++;
+            }
+            $this.attributeFilters.Patient.dcmTag.splice(index, 0, 2101760);
+
+            let pat, study, patAttrs, tags = $this.attributeFilters.Patient.dcmTag;
+            console.log('res', res);
+            res.forEach(function (studyAttrs, index) {
+                patAttrs = {};
+                $this.extractAttrs(studyAttrs, tags, patAttrs);
+                if (!(pat && _.isEqual(pat.attrs, patAttrs))) { //angular.equals replaced with Rx.helpers.defaultComparer
+                    pat = {
+                        attrs: patAttrs,
+                        studies: [],
+                        showAttributes: false
+                    };
+                    // $this.$apply(function () {
+                    $this.patients.push(pat);
+                    // });
+                }
+                study = {
+                    patient: pat,
+                    offset: offset + index,
+                    moreSeries: false,
+                    attrs: studyAttrs,
+                    series: null,
+                    showAttributes: false,
+                    fromAllStudies: false,
+                    selected: false
+                };
+                pat.studies.push(study);
+                //                   $this.studies.push(study); //sollte weg kommen
+            });
+            console.log("this pateint",this.patients);
+/*            if ($this.moreStudies = (res.length > $this.limit)) {
+                pat.studies.pop();
+                if (pat.studies.length === 0) {
+                    $this.patients.pop();
+                }
+                // this.studies.pop();
+            }*/
+            // console.log('patients=', $this.patients[0]);
+            // $this.mainservice.setMessage({
+            //     "title": "Info",
+            //     "text": "Test",
+            //     "status": "info"
+            // });
+            // sessionStorage.setItem("patients", $this.patients);
+            // $this.mainservice.setGlobal({patients:this.patients,moreStudies:$this.moreStudies});
+            // $this.mainservice.setGlobal({studyThis:$this});
+/*            console.log('global set', $this.mainservice.global);
+            $this.cfpLoadingBar.complete();*/
+        },(err)=>{
+
+        });
+    }
+
+    initAttributeFilter(entity, retries) {
+        let $this = this;
+        this.$http.get('../attribute-filter/' + entity)
+            .map(res => {let resjson; try{
+                let pattern = new RegExp("[^:]*:\/\/[^\/]*\/auth\/");
+                if(pattern.exec(res.url)){
+                    WindowRefService.nativeWindow.location = "/dcm4chee-arc/ui2/";
+                }
+                resjson = res.json(); }catch (e){resjson = {}; } return resjson; })
+            .subscribe(
+                function (res) {
+                    if (entity === 'Patient' && res.dcmTag){
+                        let privateAttr = [parseInt('77770010', 16), parseInt('77771010', 16), parseInt('77771011', 16)];
+                        res.dcmTag.push(...privateAttr);
+                    }
+                    $this.attributeFilters[entity] = res;
+                    console.log('this.attributeFilters', $this.attributeFilters);
+                    // $this.mainservice.setGlobal({attributeFilters:$this.attributeFilters});
+                },
+                function (res) {
+                    if (retries)
+                        $this.initAttributeFilter(entity, retries - 1);
+                });
+    };
 
 }
--- dcm4chee-arc-ui2/src/app/pro/migration/retrieve-export/retrieve-export.service.ts
+++ dcm4chee-arc-ui2/src/app/pro/migration/retrieve-export/retrieve-export.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {StudiesService} from "../../../studies/studies.service";
+import * as _ from 'lodash';
 
 @Injectable()
 export class RetrieveExportService {
 
-  constructor() { }
+  constructor(
+      private studieService:StudiesService
+  ) { }
 
   getRetrieveFilterSchema(aet,submitText){
     return [
@@ -142,28 +146,43 @@ export class RetrieveExportService {
             filter_block:[
                 {
                     firstChild:{
-                        tag:"label",
-                        text:"Query AETitle"
+                        tag:"checkbox",
+                        filterKey:"incomplete",
+                        text:"Only incomplete studies"
                     },
                     secondChild:{
-                        tag:"select",
-                        options:aet,
-                        showStar:true,
-                        filterKey:"QueryAET",
-                        description:"Archive AE Title to filter by"
+                        tag:"input",
+                        type:"text",
+                        filterKey:"ReferringPhysicianName",
+                        placeholder:"Referring physician"
                     }
                 },
                 {
                     firstChild:{
-                        tag:"label",
-                        text:"Destination AETitle"
+                        tag:"modality",
+                        type:"text",
+                        filterKey:"ModalitiesInStudy",
+                        placeholder:"Modality",
                     },
                     secondChild:{
-                        tag:"select",
-                        options:aet,
-                        showStar:true,
-                        filterKey:"DestinationAET",
-                        description:"Archive AE Title to filter by"
+                        tag:"input",
+                        type:"text",
+                        filterKey:"InstitutionName",
+                        placeholder:"Institution Name"
+                    }
+                },
+                {
+                    firstChild:{
+                        tag:"p-calendar",
+                        type:"string",
+                        filterKey:"studyDate.from",
+                        placeholder:"Start date from"
+                    },
+                    secondChild:{
+                        tag:"p-calendar",
+                        type:"string",
+                        filterKey:"studyDate.to",
+                        placeholder:"Start date to"
                     }
                 }
             ]
@@ -171,6 +190,112 @@ export class RetrieveExportService {
         {
             filter_block:[
                 {
+                    firstChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"SendingApplicationEntityTitleOfSeries",
+                        placeholder:"Sending Application Entity Title of Series",
+                        title:"Sending Application Entity Title of Series"
+                    },
+                    secondChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"InstitutionalDepartmentName",
+                        placeholder:"Institutional Department Name"
+                    }
+                },{
+                    firstChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"StationName",
+                        placeholder:"Station",
+                        title:"Station Name"
+                    },
+                    secondChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"SOPClassesInStudy",
+                        placeholder:"SOP classes in study",
+                        title:"SOP classes in study"
+                    }
+                },{
+                    firstChild:{
+                        tag:"checkbox",
+                        filterKey:"retrievefailed",
+                        text:"Only failed to be retrieved"
+                    },
+                    secondChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"SeriesDescription",
+                        placeholder:"Series Description",
+                        title:"Series Description"
+                    }
+                }
+            ]
+        },
+        {
+            filter_block:[
+                {
+                    firstChild:{
+                        tag:"checkbox",
+                        filterKey:"expired",
+                        text:"Only expired studies"
+                    },
+                    secondChild:{
+                        tag:"checkbox",
+                        filterKey:"incomplete",
+                        text:"Only incomplete studies"
+                    }
+                },{
+                    firstChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"StudyID",
+                        placeholder:"Study ID",
+                        title:"Study ID"
+                    },
+                    secondChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"StudyInstanceUID",
+                        placeholder:"Study Instance UID",
+                        title:"Study Instance UID"
+                    }
+                },{
+                    firstChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"StudyDescription",
+                        placeholder:"Study Description",
+                        title:"Study Description"
+                    },
+                    secondChild:{
+                        tag:"input",
+                        type:"text",
+                        filterKey:"BodyPartExamined",
+                        placeholder:"Body Part Examined",
+                        title:"Body Part Examined"
+                    }
+                }
+            ]
+        },
+        {
+            filter_block:[
+                {
+                    firstChild:{
+                        tag:"p-calendar",
+                        type:"string",
+                        filterKey:"StudyReceiveDateTime.from",
+                        placeholder:"Study created from"
+                    },
+                    secondChild:{
+                        tag:"p-calendar",
+                        type:"string",
+                        filterKey:"StudyReceiveDateTime.to",
+                        placeholder:"Study created to"
+                    }
+                },{
                     firstChild:{
                         tag:"dummy"
                     },
@@ -181,15 +306,43 @@ export class RetrieveExportService {
                 {
                     firstChild:{
                         tag:"button",
-                        text:submitText,
-                        description:"Maximal number of tasks in returned list"
+                        id:"count",
+                        text:'QUERIE COUNT',
+                        description:"QUERIE ONLY THE COUNT"
                     },
                     secondChild:{
-                        tag:"dummy"
+                        tag:"button",
+                        id:"querie",
+                        text:'QUERIE STUDIES',
+                        description:"QUERIE STUDIES"
                     }
                 }
             ]
         }
     ];
   }
+    cloneWithoutAet(object){
+      const aetsKey = [
+          "LocalAET",
+          "ExternalAET",
+          "QueryAET",
+          "DestinationAET"
+      ];
+      let newObject = _.clone(object);
+      aetsKey.forEach(aet=>{
+          delete newObject[aet];
+      })
+      return newObject;
+    }
+    rsUrl(params){
+        let initernalAet = params.LocalAET || params.QueryAET;
+        let externalAet = params.ExternalAET;
+        return this.studieService.rsURL("external",null, initernalAet, externalAet);
+    }
+    getStudiesCount(params){
+        return this.studieService.getCount(this.rsUrl(params), "studies",this.cloneWithoutAet(params));
+    }
+    getStudies(params){
+        return this.studieService.queryStudies(this.rsUrl(params), this.cloneWithoutAet(params));
+    }
 }
